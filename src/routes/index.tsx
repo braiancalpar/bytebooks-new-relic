c649bd5dd61d0fa5f2fb86fc87b2e0d5e9d1b477
braiancalpar <braian.santos@calpar.com.br>
2026-09-25 15:05:28 -0300

    adiciona rota para página 404 personalizada

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,6 +13,7 @@ export const Routes = () => (
       <Route exact path="/" component={Catalog} />
       <Route path="/book" component={BookDetail} />
       <Route path="/order" component={Order} />
+      <Route path="*" component={() => <h1>Não há nada aqui.</h1>} />
     </Switch>
     <Footer />
   </AppContext>
